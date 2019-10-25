e031fa992d510a669f27d3b073b225dda2fa5428
June Song <junesong@Junes-Air.lan>
2019-10-25 17:45:24 +0800

    update ref based

--- pipeline.R
+++ pipeline.R
@@ -1,8 +1,8 @@
 ## pipeline
-sd1=apply(y1, 1, sd, na.rm = F)
-include = which(sd1>=quantile(sd1,0.05))
-O = y1[include,]
-edata1 = edata[include,]
+# sd1=apply(y1, 1, sd, na.rm = F)
+# include = which(sd1>=quantile(sd1,0.05))
+# O = y1[include,]
+# edata1 = edata[include,]
 
 preprocess <- function(input_methyl, input_phenotype, input_covariate = NULL){
   
@@ -97,7 +97,7 @@ MethylDeconv <- function(input_methyl, input_phenotype, input_covariate = NULL, 
   if(method == "HousemanRef_based"){
     library(RefFreeEWAS)
     if(is.null(input_reference)){
-      print('Error: Reference matrix required as inputs!')
+      print('Reference matrix set as whole blood by default!')
     }
     else{
       #1. estimate cell proportions
@@ -139,42 +139,37 @@ MethylDeconv <- function(input_methyl, input_phenotype, input_covariate = NULL, 
   
   
   if(method == "450k_Ref_based"){
-    source("https://bioconductor.org/biocLite.R")
-    biocLite(c("minfi","quadprog","FlowSorted.Blood.450k",
-               "IlluminaHumanMethylation450kmanifest",
-               "IlluminaHumanMethylation450kanno.ilmn12.hg19"))
     lib = c("minfi","quadprog","FlowSorted.Blood.450k",
             "IlluminaHumanMethylation450kmanifest",
             "IlluminaHumanMethylation450kanno.ilmn12.hg19");
     lapply(lib, require, character.only = TRUE);
     
-    grSet1=read.table("input_data.txt",header=T); grSet=grSet1[,-1];
-    rownames(grSet)=grSet1[,1]; grSet=data.matrix(grSet);
-    referenceMset = get('FlowSorted.Blood.450k.compTable');
-    
-    cell = c("CD8T","CD4T", "NK","Bcell","Mono","Gran","Eos");
-    compData = minfi:::pickCompProbes(referenceMset, cellTypes=cell);
-    coefs = compData$coefEsts;
-    coefs = coefs[ intersect(rownames(grSet), rownames(coefs)), ];
-    rm(referenceMset);
-    
-    counts = minfi:::projectCellType(grSet[rownames(coefs), ], coefs);
-    rownames(counts) = colnames(grSet);
-    write.table(counts,file="Ewasher_data_minficell.csv",sep=",");
-    
-    
-    
-    
-    cell = read.csv("Ewasher_data_minficell.csv",header=T);
-    
-    cell1 = cell[,1];cell2 = cell[,3];cell3 = cell[,4];cell5 = cell[,6];
-    cell6 = cell[,7];cell7 = cell[,8];
-    
-    mod11 = model.matrix(~x1+x2+x3+x4+x5+x6+cell2+cell3+cell4+cell5+cell6+cell7);
-    fit1  = lmFit(y12,mod11,method="robust");
-    fite1 = eBayes(fit1);
-    tab1  = topTable(fite1, coef = "x1",number=length(y1[,1]), p.val=0.05,adjust = "fdr");
-    write.table(tab1,file="Ewasher_data_minfi_CpG.csv",sep=",")
+    #input_methyl
+    # grSet1=read.table("input_data.txt",header=T); grSet=grSet1[,-1];
+    # rownames(grSet)=grSet1[,1]; grSet=data.matrix(grSet)
+    # 
+    if(is.null(input_reference)){
+      referenceMset = get('FlowSorted.Blood.450k.compTable')
+      cell = c("CD8T","CD4T", "NK","Bcell","Mono","Gran","Eos")
+      compData = minfi:::pickCompProbes(referenceMset, cellTypes=cell)
+      coefs = compData$coefEsts
+      coefs = coefs[intersect(rownames(input_methyl),rownames(coefs)),]
+      rm(referenceMset)
+      counts = minfi:::projectCellType(input_methyl[rownames(coefs), ], coefs)
+      rownames(counts) = colnames(input_methyl)
+      retun(counts)
+    }
+  
+    ## adjust for cell proportions
+    # cell =  counts
+    # cell1 = cell[,1];cell2 = cell[,3];cell3 = cell[,4];cell5 = cell[,6];
+    # cell6 = cell[,7];cell7 = cell[,8];
+    # mod11 = model.matrix(~x1+x2+x3+x4+x5+x6+cell2+cell3+cell4+cell5+cell6+cell7);
+    # fit1  = lmFit(y12,mod11,method="robust");
+    # fite1 = eBayes(fit1);
+    # tab1  = topTable(fite1, coef = "x1",number=length(y1[,1]), p.val=0.05,adjust = "fdr");
+    # write.table(tab1,file="Ewasher_data_minfi_CpG.csv",sep=",")
+    # 
   }
   
   if(method == "RUV"){
