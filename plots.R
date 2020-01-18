769b591d7234058bc38f2d1fae7f9b665062a42a
June Song <junesong@Junes-Air.lan>
2020-01-18 21:20:52 +0800

    add venn plot

--- plots.R
+++ plots.R
@@ -10,3 +10,10 @@ EpiDISH <- rownames(centDHSbloodDMC.m)
 library(FlowSorted.Blood.450k)
 data("FlowSorted.Blood.450k.JaffeModelPars")
 Jaffe450k <- rownames(FlowSorted.Blood.450k.JaffeModelPars)
+
+library(VennDiagram)
+grid.newpage()
+draw.triple.venn(area1 = 450, area2 = 333, area3 = 600, n12 = 13, n23 = 110, n13 = 54, 
+                 n123 = 7, category = c("EPIC", "EpiDISH", "Jaffe450k"), lty = "blank", 
+                 fill = c("skyblue", "pink1", "mediumorchid"))
+
