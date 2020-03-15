e991af0cf59705438c89a644277513e23db2b5ea
June Song <junesong@andrewpc.cshl.edu>
2020-03-15 17:33:30 -0400

    add code

---
+++ TCGA_CBS_mRNAvsMethylCollapse.R
@@ -0,0 +1,77 @@
+disease <- "BRCA"
+methyl_path <- paste0("/sonas-hs/krasnitz/hpc/data/pfproj/tcga_data/tcga_methyl/", disease, "_methyl.RData")
+mRNA_path <- paste0("/sonas-hs/krasnitz/hpc/data/pfproj/tcga_data/tcga_mrna/", disease, "_mrna.RData")
+load(methyl_path)
+load(mRNA_path)
+sample_shared <- intersect(substr(colnames(BetaMatrix),1,19),substr(colnames(mRNAMatrix),1,19))
+colnames(BetaMatrix) <- substr(colnames(BetaMatrix),1,19)
+colnames(mRNAMatrix) <- substr(colnames(mRNAMatrix),1,19)
+BetaMatrix <- BetaMatrix[,sample_shared]  #485577    253
+mRNAMatrix <- mRNAMatrix[,sample_shared]  #17814   253
+
+### impute the missing values in mRNA matrix
+mRNAMatrix_nona <- apply(mRNAMatrix,1,function(x){x[which(is.na(x)==TRUE)] <- mean(na.omit(x)); return(x)})
+mRNAMatrix_nona <- t(mRNAMatrix_nona)
+
+
+sig_matrix <- read.table("/sonas-hs/wigler/hpc/home/jsong/MethylDeConv/LM22.txt",header=T,sep="\t",row.names=1,check.names=F)
+
+source('/sonas-hs/wigler/hpc/home/jsong/MethylDeConv/CIBERSORT_custom.R')
+
+## CIBERSORT on the mRNA data
+res_mRNA_cibersort <- CIBERSORT(sig_matrix, mRNAMatrix_nona)
+
+
+
+### collapsing
+BetaMatrix_average <- CollapseCpGsGenes(BetaMatrix, include = "all")
+BetaMatrix_maxvar <- CollapseCpGsGenes(BetaMatrix, method = "maxvar", include = "all")
+BetaMatrix_pca <- CollapseCpGsGenes(BetaMatrix, method = "PCA", include = "all")
+
+BetaMatrix_average_nona <- apply(BetaMatrix_average,1,function(x){x[which(is.na(x)==TRUE)] <- mean(na.omit(x)); return(x)})
+BetaMatrix_average_nona <- t(BetaMatrix_average_nona)
+BetaMatrix_maxvar_nona <- apply(BetaMatrix_maxvar,1,function(x){x[which(is.na(x)==TRUE)] <- mean(na.omit(x)); return(x)})
+BetaMatrix_maxvar_nona <- t(BetaMatrix_maxvar_nona)
+BetaMatrix_pca_nona <- apply(BetaMatrix_pca,1,function(x){x[which(is.na(x)==TRUE)] <- mean(na.omit(x)); return(x)})
+BetaMatrix_pca_nona <- t(BetaMatrix_pca_nona)
+
+BetaMatrix_average_TSS200 <- CollapseCpGsGenes(BetaMatrix, include = "TSS200")
+BetaMatrix_maxvar_TSS200 <- CollapseCpGsGenes(BetaMatrix, method = "maxvar", include = "TSS200")
+BetaMatrix_pca_TSS200 <- CollapseCpGsGenes(BetaMatrix, method = "PCA", include = "TSS200")
+
+BetaMatrix_average_TSS200_nona <- apply(BetaMatrix_average_TSS200,1,function(x){x[which(is.na(x)==TRUE)] <- mean(na.omit(x)); return(x)})
+BetaMatrix_average_TSS200_nona  <- t(BetaMatrix_average_TSS200_nona)
+BetaMatrix_maxvar_TSS200_nona <- apply(BetaMatrix_maxvar_TSS200,1,function(x){x[which(is.na(x)==TRUE)] <- mean(na.omit(x)); return(x)})
+BetaMatrix_maxvar_TSS200_nona <- t(BetaMatrix_maxvar_TSS200_nona)
+BetaMatrix_pca_TSS200_nona <- apply(BetaMatrix_pca_TSS200,1,function(x){x[which(is.na(x)==TRUE)] <- mean(na.omit(x)); return(x)})
+BetaMatrix_pca_TSS200_nona <- t(BetaMatrix_pca_TSS200_nona)
+
+BetaMatrix_average_TSS <- CollapseCpGsGenes(BetaMatrix, include = "TSS200&TSS1500")
+BetaMatrix_maxvar_TSS <- CollapseCpGsGenes(BetaMatrix, method = "maxvar", include = "TSS200&TSS1500")
+BetaMatrix_pca_TSS <- CollapseCpGsGenes(BetaMatrix, method = "PCA", include = "TSS200&TSS1500")
+
+BetaMatrix_average_TSS_nona <- apply(BetaMatrix_average_TSS,1,function(x){x[which(is.na(x)==TRUE)] <- mean(na.omit(x)); return(x)})
+BetaMatrix_average_TSS_nona  <- t(BetaMatrix_average_TSS_nona)
+BetaMatrix_maxvar_TSS_nona <- apply(BetaMatrix_maxvar_TSS,1,function(x){x[which(is.na(x)==TRUE)] <- mean(na.omit(x)); return(x)})
+BetaMatrix_maxvar_TSS_nona <- t(BetaMatrix_maxvar_TSS_nona)
+BetaMatrix_pca_TSS_nona <- apply(BetaMatrix_pca_TSS,1,function(x){x[which(is.na(x)==TRUE)] <- mean(na.omit(x)); return(x)})
+BetaMatrix_pca_TSS_nona <- t(BetaMatrix_pca_TSS_nona)
+
+
+
+res_collapse_average_all_cibersort <- cibersortCIBERSORT(sig_matrix, BetaMatrix_average_nona)
+res_collapse_maxvar_all_cibersort <- CIBERSORT(sig_matrix, BetaMatrix_maxvar_nona)
+res_collapse_pca_all_cibersort <- CIBERSORT(sig_matrix, BetaMatrix_pca_nona)
+
+res_collapse_average_TSS200_cibersort <- CIBERSORT(sig_matrix, BetaMatrix_average_TSS200_nona)
+res_collapse_maxvar_TSS200_cibersort <- CIBERSORT(sig_matrix, BetaMatrix_maxvar_TSS200_nona)
+res_collapse_pca_TSS200_cibersort <- CIBERSORT(sig_matrix, BetaMatrix_pca_TSS200_nona)
+
+res_collapse_average_TSS_cibersort <- CIBERSORT(sig_matrix, BetaMatrix_average_TSS_nona)
+res_collapse_maxvar_TSS_cibersort <- CIBERSORT(sig_matrix, BetaMatrix_maxvar_TSS_nona)
+res_collapse_pca_TSS_cibersort <- CIBERSORT(sig_matrix, BetaMatrix_pca_TSS_nona)
+
+save("res_mRNA_cibersort", "res_collapse_average_all_cibersort", "res_collapse_maxvar_all_cibersort",
+     "res_collapse_pca_all_cibersort", "res_collapse_average_TSS200_cibersort", "res_collapse_maxvar_TSS200_cibersort",
+     "res_collapse_pca_TSS200_cibersort", "res_collapse_average_TSS_cibersort","res_collapse_maxvar_TSS_cibersort",
+     "res_collapse_pca_TSS_cibersort", file = "/sonas-hs/wigler/hpc/home/jsong/MethylDeConv/brcaCIBERSORTmRNAvsMethylCollapsed.RData")
