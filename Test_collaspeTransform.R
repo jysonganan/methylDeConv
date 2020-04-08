340ca83245d7f386267efb10c4bffe0acad26ccc
jysong <jysongane@gmail.com>
2020-04-07 23:54:03 -0400

    Update Test_collaspeTransform.R

--- Test_collaspeTransform.R
+++ Test_collaspeTransform.R
@@ -14,7 +14,8 @@ print(dim(RNAMatrix))
 
 source("/sonas-hs/wigler/hpc/home/jsong/MethylDeConv/CollapseCpGsGenes.R")
 BetaMatrix_average <- CollapseCpGsGenes(BetaMatrix, include = "all")
-
+BetaMatrix_average_nearest <- CollapseCpGsGenes(BetaMatrix, NearestToCpG = TRUE)
+BetaMatrix_average_nearest_TSS <- CollapseCpGsGenes(BetaMatrix, NearestToCpG = TRUE, NearestMethod = "TSS")
 
 genes_all <- rownames(RNAMatrix)
 genes_all <- gsub("\\|.*","", genes_all)
@@ -27,8 +28,18 @@ rownames(RNAMatrix) <- genes_all[-(1:29)]
 sig_matrix <- read.table("/sonas-hs/wigler/hpc/home/jsong/MethylDeConv/LM22.txt",header=T,sep="\t",row.names=1,check.names=F)
 
 
-shared_genes <- intersect(intersect(rownames(sig_matrix),rownames(RNAMatrix)), rownames(BetaMatrix_average))
+shared_genes_average <- intersect(intersect(rownames(sig_matrix),rownames(RNAMatrix)), rownames(BetaMatrix_average))
 #502
+shared_genes_average_nearest <- intersect(intersect(rownames(sig_matrix),rownames(RNAMatrix)), rownames(BetaMatrix_average_nearest))
+shared_genes_average_nearest_TSS  <- intersect(intersect(rownames(sig_matrix),rownames(RNAMatrix)), rownames(BetaMatrix_average_nearest_TSS))
+
+BetaMatrix_average <- BetaMatrix_average[shared_genes_average,]
+BetaMatrix_average_nearest <- BetaMatrix_average_nearest[shared_genes_average_nearest,]
+BetaMatrix_average_nearest_TSS <- BetaMatrix_average_nearest_TSS[shared_genes_average_nearest_TSS,]
+
+save("BetaMatrix_average", "BetaMatrix_average_nearest","BetaMatrix_average_nearest_TSS","RNAMatrix",file = "KICHforGP.RData")
+
+
 corr <- rep(NA, length(shared_genes))
 for (i in 1:length(shared_genes)){
   gene <- shared_genes[i]
@@ -45,19 +56,4 @@ for (i in 1:dim(BetaMatrix)[2]){
 
 
 
-### check the logit transform
-BetaMatrix_logit <- -log(BetaMatrix_average/(1-BetaMatrix_average))
-
-
-corr <- rep(NA, length(shared_genes))
-for (i in 1:length(shared_genes)){
-  gene <- shared_genes[i]
-  corr[i] <- cor(RNAMatrix[gene,],as.numeric(BetaMatrix_logit[gene,]))
-}
-
-
-corr <- rep(NA, dim(BetaMatrix)[2])
-for (i in 1:dim(BetaMatrix)[2]){
-  corr[i] <- cor(RNAMatrix[shared_genes,i],BetaMatrix_logit[shared_genes,i],method = "spearman")
-}
 
