58946e5c5ed9ef006e0de7349be6340af1a583c7
jysong <jysongane@gmail.com>
2020-05-28 16:33:36 -0400

    Update Analysis_matched450kEPIC.R

--- Analysis_matched450kEPIC.R
+++ Analysis_matched450kEPIC.R
@@ -27,18 +27,54 @@ library(minfi)
 geoMat <- getGEO("GSE111165")
 
 
+tissue = "saliva"
 
+
+if (tissue == "blood"){    ## 6 matched samples subject:101,102,103,104,105,106
+  pD.450k <- pData(geoMat[[1]])
+  pD.EPIC <- pData(geoMat[[2]])
+  common_subject_id <- intersect(pD.450k[pD.450k[,"tissue:ch1"]=="blood","subject:ch1"], 
+                                 pD.EPIC[pD.EPIC[,"tissue:ch1"]=="blood","subject:ch1"])
+  matched_samples_450k <- rownames(pD.450k[pD.450k[,"tissue:ch1"]=="blood"&pD.450k[,"subject:ch1"]%in%common_subject_id,])
+  matched_samples_EPIC <- rownames(pD.EPIC[pD.EPIC[,"tissue:ch1"]=="blood"&pD.EPIC[,"subject:ch1"]%in%common_subject_id,])
+}
+
+if (tissue == "saliva"){ ## 6 matched samples subject:101,102,103,104,105,106
+  pD.450k <- pData(geoMat[[1]])
+  pD.EPIC <- pData(geoMat[[2]])
+  common_subject_id <- intersect(pD.450k[pD.450k[,"tissue:ch1"]=="saliva","subject:ch1"], 
+                                 pD.EPIC[pD.EPIC[,"tissue:ch1"]=="saliva","subject:ch1"])
+  matched_samples_450k <- rownames(pD.450k[pD.450k[,"tissue:ch1"]=="saliva"&pD.450k[,"subject:ch1"]%in%common_subject_id,])
+  matched_samples_EPIC <- rownames(pD.EPIC[pD.EPIC[,"tissue:ch1"]=="saliva"&pD.EPIC[,"subject:ch1"]%in%common_subject_id,])
+}
+
+if (tissue == "brain_whole"){ ## matched samples 14 vs 6 (there are replicates with same subject id in 450k) ## make brain region
+  ## also matched; then matched samples: 6 subject:101,102,103,104,105,106
+  pD.450k <- pData(geoMat[[1]])
+  pD.EPIC <- pData(geoMat[[2]])
+  common_subject_id <- intersect(pD.450k[pD.450k[,"tissue:ch1"]=="brain_whole","subject:ch1"], 
+                                 pD.EPIC[pD.EPIC[,"tissue:ch1"]=="brain_whole","subject:ch1"])
+  matched_samples_EPIC <- rownames(pD.EPIC[pD.EPIC[,"tissue:ch1"]=="brain_whole"&pD.EPIC[,"subject:ch1"]%in%common_subject_id,])
+  matched_samples_450k <- rep(NA, length(matched_samples_EPIC))
+  for (i in 1:length(matched_samples_EPIC)){
+    subject <- pD.EPIC[matched_samples_EPIC, "subject:ch1"][i]
+    brain_region <- as.character(pD.EPIC[matched_samples_EPIC, "characteristics_ch1.5"])[i]
+    matched_samples_450k[i] <- rownames(pD.450k[pD.450k[,"subject:ch1"] == subject & substr(as.character(pD.450k[,"characteristics_ch1.5"]),1,18) == substr(brain_region,1,18),])
+}}
+
+
+
+
+
+## GSE111165 
 ## for 450k   46 samples
-pD.all <- pData(geoMat[[1]])
-
 rgSet<- read.metharray.exp("GSE111165/idat/450k",force = TRUE)
 grSet <- preprocessNoob(rgSet, dyeMethod = "single")
 betaMat <- getBeta(grSet)
 colnames(betaMat) <- substr(colnames(betaMat),1,10)
-betaMat <- betaMat[,rownames(pD.all)]
+betaMat <- betaMat[,matched_samples_450k]
 
 
-data_type = "Flow450k"
 library(FlowSorted.Blood.450k)
 CellLines.matrix = NULL
 cellTypes = c("CD8T", "CD4T", "NK", "Bcell", "Mono", "Gran")
@@ -75,7 +111,7 @@ rownames(multiGlmnet_predProb) <- colnames(betaMat)
 
 
 save("Houseman_res","RPC_res","CBS_res","Houseman_res_glmnetpreselect","RPC_res_glmnetpreselect",
-     "CBS_res_glmnetpreselect", "multiGlmnet_predProb", file = "Match450kRes.RData")
+     "CBS_res_glmnetpreselect", "multiGlmnet_predProb", file = paste0(tissue,"Match450kRes.RData"))
 
 
 
@@ -83,17 +119,15 @@ save("Houseman_res","RPC_res","CBS_res","Houseman_res_glmnetpreselect","RPC_res_
 
 
 
-## for EPIC 101 samples
-pD.all <- pData(geoMat[[2]])
-
+## for EPIC 101 samples   EPIC-epithelial 
 rgSet<- read.metharray.exp("GSE111165/idat",force = TRUE)
 grSet <- preprocessNoob(rgSet, dyeMethod = "single")
 betaMat <- getBeta(grSet)
 colnames(betaMat) <- substr(colnames(betaMat),1,10)
-betaMat <- betaMat[,rownames(pD.all)]
+betaMat <- betaMat[,matched_samples_EPIC]
 
 
-data_type = "FlowEPIC_Epithelial"
+
 library(GEOquery)
 library(minfi)
 CellLines.matrix = NULL
@@ -121,16 +155,86 @@ load("FlowEPIC_EpithelialProbesdefault.RData")
 probes_select <- probes_oneVsAllttest
 library(EpiDISH)
 source("projectCellType.R")
-Houseman_res_epic <- projectCellType(betaMat[probes_select,],as.matrix(compTable[probes_select,3:10]))
-RPC_res_epic <- epidish(betaMat, as.matrix(compTable[probes_select,3:10]), method = "RPC")$estF
-CBS_res_epic <- epidish(betaMat, as.matrix(compTable[probes_select,3:10]), method = "CBS")$estF
+Houseman_res_epicEpithelial <- projectCellType(betaMat[probes_select,],as.matrix(compTable[probes_select,3:10]))
+RPC_res_epicEpithelial <- epidish(betaMat, as.matrix(compTable[probes_select,3:10]), method = "RPC")$estF
+CBS_res_epicEpithelial <- epidish(betaMat, as.matrix(compTable[probes_select,3:10]), method = "CBS")$estF
 
 load("FlowEPIC_EpithelialProbePreselect_multiclassGlmnet.RData")
 #### the deconv + Glmnet on preselected 1800 probes -> select ~922 probes in glmnet
 probes_select <- ProbePreselect_multiclassGlmnet[[1]][-1]
-Houseman_res_glmnetpreselect <- projectCellType(betaMat[probes_select,],as.matrix(compTable[probes_select,3:10]))
-RPC_res_glmnetpreselect_epic <- epidish(betaMat, as.matrix(compTable[probes_select,3:10]), method = "RPC")$estF
-CBS_res_glmnetpreselect_epic <- epidish(betaMat, as.matrix(compTable[probes_select,3:10]), method = "CBS")$estF
+Houseman_res_glmnetpreselect_epicEpithelial <- projectCellType(betaMat[probes_select,],as.matrix(compTable[probes_select,3:10]))
+RPC_res_glmnetpreselect_epicEpithelial <- epidish(betaMat, as.matrix(compTable[probes_select,3:10]), method = "RPC")$estF
+CBS_res_glmnetpreselect_epicEpithelial <- epidish(betaMat, as.matrix(compTable[probes_select,3:10]), method = "CBS")$estF
+
+probes <- ref_probe_selection_oneVsAllttest(ref_betamatrix, ref_phenotype,probeSelect = "both", MaxDMRs = 300)
+library(dplyr)
+multiGlmnet_predProb_epicEpithelial <- predict(ProbePreselect_multiclassGlmnet[[2]], newdata = t(betaMat[probes,]), type = "prob") %>% 
+  mutate('class'=names(.)[apply(., 1, which.max)])
+rownames(multiGlmnet_predProb_epicEpithelial) <- colnames(betaMat)
+
+save("Houseman_res_epicEpithelial","RPC_res_epicEpithelial","CBS_res_epicEpithelial","Houseman_res_glmnetpreselect_epicEpithelial","RPC_res_glmnetpreselect_epicEpithelial",
+     "CBS_res_glmnetpreselect_epicEpithelial", "multiGlmnet_predProb_epicEpithelial", file = paste0(tissue, "MatchEPICEpithelialRes.RData"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## for EPIC 101 samples   FlowEPIC
+rgSet<- read.metharray.exp("GSE111165/idat",force = TRUE)
+grSet <- preprocessNoob(rgSet, dyeMethod = "single")
+betaMat <- getBeta(grSet)
+colnames(betaMat) <- substr(colnames(betaMat),1,10)
+betaMat <- betaMat[,matched_samples_EPIC]
+
+
+
+library(GEOquery)
+library(minfi)
+CellLines.matrix = NULL
+cellTypes = c("CD8T", "CD4T", "NK", "Bcell", "Mono", "Neu")
+load("FlowSorted.Blood.EPIC.RData")
+ref_betamatrix <- getBeta(preprocessNoob(FlowSorted.Blood.EPIC, dyeMethod = "single"))
+ref_phenotype <- as.data.frame(colData(FlowSorted.Blood.EPIC))$CellType
+keep <- which(ref_phenotype %in% cellTypes)
+ref_betamatrix <- ref_betamatrix[,keep]
+ref_phenotype <- ref_phenotype[keep]
+source("refCompTableProbeSelection.R")
+compTable <- ref_compTable(ref_betamatrix, ref_phenotype)
+
+
+
+load("FlowEPICProbesdefault.RData")
+probes_select <- probes_oneVsAllttest
+library(EpiDISH)
+source("projectCellType.R")
+Houseman_res_epic <- projectCellType(betaMat[probes_select,],as.matrix(compTable[probes_select,3:8]))
+RPC_res_epic <- epidish(betaMat, as.matrix(compTable[probes_select,3:8]), method = "RPC")$estF
+CBS_res_epic <- epidish(betaMat, as.matrix(compTable[probes_select,3:8]), method = "CBS")$estF
+
+load("FlowEPICProbePreselect_multiclassGlmnet.RData")
+#### the deconv + Glmnet on preselected 1800 probes -> select ~922 probes in glmnet
+probes_select <- ProbePreselect_multiclassGlmnet[[1]][-1]
+Houseman_res_glmnetpreselect_epic <- projectCellType(betaMat[probes_select,],as.matrix(compTable[probes_select,3:8]))
+RPC_res_glmnetpreselect_epic <- epidish(betaMat, as.matrix(compTable[probes_select,3:8]), method = "RPC")$estF
+CBS_res_glmnetpreselect_epic <- epidish(betaMat, as.matrix(compTable[probes_select,3:8]), method = "CBS")$estF
 
 probes <- ref_probe_selection_oneVsAllttest(ref_betamatrix, ref_phenotype,probeSelect = "both", MaxDMRs = 300)
 library(dplyr)
@@ -139,4 +243,4 @@ multiGlmnet_predProb_epic <- predict(ProbePreselect_multiclassGlmnet[[2]], newda
 rownames(multiGlmnet_predProb_epic) <- colnames(betaMat)
 
 save("Houseman_res_epic","RPC_res_epic","CBS_res_epic","Houseman_res_glmnetpreselect_epic","RPC_res_glmnetpreselect_epic",
-     "CBS_res_glmnetpreselect_epic", "multiGlmnet_predProb_epic", file = "MatchEPICRes.RData")
+     "CBS_res_glmnetpreselect_epic", "multiGlmnet_predProb_epic", file = paste0(tissue, "MatchEPICRes.RData"))
