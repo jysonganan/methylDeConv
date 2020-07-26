ddacb0654346b7017d1c76726390095d23481cb8
jysong <jysongane@gmail.com>
2020-07-26 17:14:45 -0400

    Update BetaMixture_sim.R

--- BetaMixture_sim.R
+++ BetaMixture_sim.R
@@ -1,6 +1,4 @@
 
-
-
 #https://stats.stackexchange.com/questions/12232/calculating-the-parameters-of-a-beta-distribution-using-the-mean-and-variance
 
 estBetaParams <- function(mu, var) {
@@ -17,13 +15,14 @@ estBetaParams_datRow <- function(datRow){
 
 betaSim <- function(ref_betamatrix, ref_phenotype){
   phenotype <- levels(as.factor(ref_phenotype))
-  dat_sim <- matrix(NA, nrows(ref_betamatrix), length(phenotype))
+  dat_sim <- matrix(NA, nrow(ref_betamatrix), length(phenotype))
   for (i in 1:length(phenotype)){
     dat <- ref_betamatrix[,ref_phenotype == phenotype[i]]
     dat_sim[,i] <- apply(dat, 1, function(x){
       para <- estBetaParams_datRow(x)
-      return(rbeta(para[[1]], para[[2]]))
+      return(rbeta(1, para[[1]], para[[2]]))
     })
   }
   return(dat_sim)
 }
+
