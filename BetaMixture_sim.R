4ad1181c14430245e264f28d1970fa36bc92c5b6
jysong <jysongane@gmail.com>
2020-07-26 18:29:56 -0400

    Update BetaMixture_sim.R

--- BetaMixture_sim.R
+++ BetaMixture_sim.R
@@ -1,4 +1,5 @@
 
+
 #https://stats.stackexchange.com/questions/12232/calculating-the-parameters-of-a-beta-distribution-using-the-mean-and-variance
 
 estBetaParams <- function(mu, var) {
@@ -23,6 +24,49 @@ betaSim <- function(ref_betamatrix, ref_phenotype){
       return(rbeta(1, para[[1]], para[[2]]))
     })
   }
+  rownames(dat_sim) <- rownames(ref_betamatrix)
+  colnames(dat_sim) <- phenotype
   return(dat_sim)
+}
+
+
+sim1 <- betaSim(ref_betamatrix, ref_phenotype)
+
+set.seed(2)
+purified_datasets_sim <- list()
+for (i in 1:20){
+  purified_datasets_sim[[i]] <-  betaSim(ref_betamatrix, ref_phenotype)
+}
+
+## impute NAs with zeros
+
+## Within in each of three groups (no epithelial, low epithelial and high epithelial)
+## generate 600 mixture samples:
+
+## combined the 20 ramdomly generated purified datasets (beta distribution) 
+## with 30 randomly generated probablities to generate 600 mixture samples.
+
+
+## 1. no epithelial
+set.seed(3)
+proportions_sim <- rdirichlet(30, c(1,1,1,1,1,1))
+proportions_sim <- cbind(proportions_sim, 0)
+true_proportions_sim <- proportions_sim
+for (i in 1:19){
+  true_proportions_sim <- rbind(true_proportions_sim, proportions_sim)
+}
+
+mixture_sim <- list()
+for (i in 1:20){
+  for (j in 1:30){
+    res = true_proportions_sim[j,] * t(purified_datasets_sim[[i]])
+    res = apply(res, 2, sum)
+    mixture_sim <- c(mixture_sim, res)
+  }
+}
+
+mixture_sim_mat <- matrix(NA, nrow(ref_betamatrix), 600)
+for (i in 1:600){
+  mixture_sim_mat[,i] <- mixture_sim[[i]]
 }
 
