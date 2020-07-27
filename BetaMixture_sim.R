19b95161c039f65e1dcc2cfd6677ece2ff1e0a88
jysong <jysongane@gmail.com>
2020-07-27 02:01:25 -0400

    Update BetaMixture_sim.R

--- BetaMixture_sim.R
+++ BetaMixture_sim.R
@@ -153,3 +153,88 @@ mixture_sim_mat_highEpithelial <- mixture_sim_mat
 save("true_proportions_sim_highEpithelial", "true_proportions_sim_lowEpithelial", 
      "true_proportions_sim_noEpithelial", "mixture_sim_mat_highEpithelial", 
      "mixture_sim_mat_lowEpithelial", "mixture_sim_mat_noEpithelial", file = "mixture_datasets_sim.RData")
+
+
+
+
+
+
+
+
+
+##### analysis
+# 1. using EPIC reference
+load("FlowEPICProbesdefault.RData")
+load("FlowEPICProbePreselect_multiclassGlmnet.RData")
+source("refCompTableProbeSelection.R")
+compTable <- ref_compTable(ref_betamatrix, ref_phenotype)
+
+probes <- probes_oneVsAllttest
+benchmark_betamatrix <- mixture_sim_mat_highEpithelial
+
+
+probes_select <- intersect(probes, rownames(benchmark_betamatrix))  ## 600
+library(EpiDISH)
+source("projectCellType.R")
+Houseman_res <- projectCellType(benchmark_betamatrix[probes_select,],as.matrix(compTable[probes_select,3:8]))
+RPC_res <- epidish(benchmark_betamatrix, as.matrix(compTable[probes_select,3:8]), method = "RPC")$estF
+CBS_res <- epidish(benchmark_betamatrix, as.matrix(compTable[probes_select,3:8]), method = "CBS")$estF
+
+corr <- rep(NA, 600)
+for (i in 1:600){
+  corr[i] <- cor(true_proportions_sim_highEpithelial[i,c("Bcell", "CD4T","CD8T","Mono","Neu","NK")], CBS_res[i,], method = "spearman")
+}
+
+
+corr <- rep(NA, 6)
+for (i in 1:6){
+  corr[i] <- cor(true_proportions_sim_highEpithelial[,c("Bcell", "CD4T","CD8T","Mono","Neu","NK")][,i], RPC_res[,i], method = "spearman")
+}
+
+
+
+#2 use EPIC + Epithelial reference
+load("FlowEPIC_Epithelial_nocfDNAProbesdefault.RData")
+load("FlowEPIC_Epithelial_nocfDNAProbePreselect_multiclassGlmnet.RData")
+source("refCompTableProbeSelection.R")
+compTable <- ref_compTable(ref_betamatrix, ref_phenotype)
+
+probes <- ProbePreselect_multiclassGlmnet[[1]][-1]
+benchmark_betamatrix <- mixture_sim_mat_highEpithelial
+
+
+probes_select <- intersect(probes, rownames(benchmark_betamatrix))  ## 600
+library(EpiDISH)
+source("projectCellType.R")
+Houseman_res <- projectCellType(benchmark_betamatrix[probes_select,],as.matrix(compTable[probes_select,3:9]))
+RPC_res <- epidish(benchmark_betamatrix, as.matrix(compTable[probes_select,3:9]), method = "RPC")$estF
+CBS_res <- epidish(benchmark_betamatrix, as.matrix(compTable[probes_select,3:9]), method = "CBS")$estF
+
+corr <- rep(NA, 600)
+for (i in 1:600){
+  corr[i] <- cor(true_proportions_sim_highEpithelial[i,c("Bcell", "CD4T","CD8T","Mono","Neu","NK")], CBS_res[i,c("Bcell", "CD4T","CD8T","Mono","Neu","NK")], method = "spearman")
+}
+
+
+corr <- rep(NA, 6)
+for (i in 1:6){
+  corr[i] <- cor(true_proportions_sim_highEpithelial[,c("Bcell", "CD4T","CD8T","Mono","Neu","NK")][,i], RPC_res[,c("Bcell", "CD4T","CD8T","Mono","Neu","NK")][,i], method = "spearman")
+}
+
+
+
+
+corr <- rep(NA, 600)
+for (i in 1:600){
+    corr[i] <- cor(true_proportions_sim_highEpithelial[i,c("Bcell", "CD4T","CD8T","Mono","Neu","NK","Epithelial")], CBS_res[i,c("Bcell", "CD4T","CD8T","Mono","Neu","NK", "Epithelial")], method = "spearman")
+}
+
+
+corr <- rep(NA, 7)
+for (i in 1:7){
+  corr[i] <- cor(true_proportions_sim_highEpithelial[,c("Bcell", "CD4T","CD8T","Mono","Neu","NK", "Epithelial")][,i], RPC_res[,c("Bcell", "CD4T","CD8T","Mono","Neu","NK", "Epithelial")][,i], method = "spearman")
+}
+
+
+
+### 3 two-stage deconvolution for high epithelial situation
