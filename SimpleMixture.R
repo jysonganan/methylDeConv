797f37142f2fa8df621634958e386781ac60f142
jysong <jysongane@gmail.com>
2020-08-03 10:22:38 -0400

    Update SimpleMixture.R

--- SimpleMixture.R
+++ SimpleMixture.R
@@ -3,18 +3,23 @@
 purified_datasets <- as.matrix(compTable[,3:9])
 
 
+
 library(MCMCpack)
 
 ## 1. no epithelial
 set.seed(3)
-
-set.seed(2)
 proportions_sim <- rdirichlet(600, c(1,1,1,1,1,1))
 proportions_sim <- cbind(proportions_sim, 0)
 proportions_sim <- proportions_sim[,c(1,2,3,7,4,5,6)]
 
 
-mixture_sim_mat <- purified_datasets %*% t(proportions_sim)
+mixture_sim_mat <- matrix(NA, nrow(purified_datasets), 600)
+for (i in 1:nrow(purified_datasets)){
+  mixture_sim_mat[i,] <- purified_datasets[i,] %*% t(proportions_sim)
+}
+
+rownames(mixture_sim_mat) <- rownames(purified_datasets)
+#mixture_sim_mat <- purified_datasets %*% t(proportions_sim)
 colnames(proportions_sim) <- colnames(purified_datasets) 
 
 
@@ -28,8 +33,17 @@ proportions_sim <- proportions_sim * (1-proportions_sim_epithelial)
 proportions_sim <- cbind(proportions_sim, proportions_sim_epithelial)
 proportions_sim <- proportions_sim[,c(1,2,3,7,4,5,6)]
 
-mixture_sim_mat <- purified_datasets %*% t(proportions_sim)
+mixture_sim_mat <- matrix(NA, nrow(purified_datasets), 600)
+for (i in 1:nrow(purified_datasets)){
+  mixture_sim_mat[i,] <- purified_datasets[i,] %*% t(proportions_sim)
+}
+
+rownames(mixture_sim_mat) <- rownames(purified_datasets)
+#mixture_sim_mat <- purified_datasets %*% t(proportions_sim)
 colnames(proportions_sim) <- colnames(purified_datasets) 
+
+
+
 
 ## 3. 
 set.seed(2)
@@ -39,8 +53,16 @@ proportions_sim <- proportions_sim * (1-proportions_sim_epithelial)
 proportions_sim <- cbind(proportions_sim, proportions_sim_epithelial)
 proportions_sim <- proportions_sim[,c(1,2,3,7,4,5,6)]
 
-mixture_sim_mat <- purified_datasets %*% t(proportions_sim)
+mixture_sim_mat <- matrix(NA, nrow(purified_datasets), 600)
+for (i in 1:nrow(purified_datasets)){
+  mixture_sim_mat[i,] <- purified_datasets[i,] %*% t(proportions_sim)
+}
+
+rownames(mixture_sim_mat) <- rownames(purified_datasets)
+#mixture_sim_mat <- purified_datasets %*% t(proportions_sim)
 colnames(proportions_sim) <- colnames(purified_datasets) 
+
+
 
 # 4
 set.seed(5)
@@ -50,7 +72,13 @@ proportions_sim <- proportions_sim * (1-proportions_sim_epithelial)
 proportions_sim <- cbind(proportions_sim, proportions_sim_epithelial)
 proportions_sim <- proportions_sim[,c(1,2,3,7,4,5,6)]
 
-mixture_sim_mat <- purified_datasets %*% t(proportions_sim)
+mixture_sim_mat <- matrix(NA, nrow(purified_datasets), 600)
+for (i in 1:nrow(purified_datasets)){
+  mixture_sim_mat[i,] <- purified_datasets[i,] %*% t(proportions_sim)
+}
+
+rownames(mixture_sim_mat) <- rownames(purified_datasets)
+#mixture_sim_mat <- purified_datasets %*% t(proportions_sim)
 colnames(proportions_sim) <- colnames(purified_datasets) 
 
 
@@ -62,7 +90,13 @@ proportions_sim <- proportions_sim * (1-proportions_sim_epithelial)
 proportions_sim <- cbind(proportions_sim, proportions_sim_epithelial)
 proportions_sim <- proportions_sim[,c(1,2,3,7,4,5,6)]
 
-mixture_sim_mat <- purified_datasets %*% t(proportions_sim)
+mixture_sim_mat <- matrix(NA, nrow(purified_datasets), 600)
+for (i in 1:nrow(purified_datasets)){
+  mixture_sim_mat[i,] <- purified_datasets[i,] %*% t(proportions_sim)
+}
+
+rownames(mixture_sim_mat) <- rownames(purified_datasets)
+#mixture_sim_mat <- purified_datasets %*% t(proportions_sim)
 colnames(proportions_sim) <- colnames(purified_datasets) 
 
 
@@ -83,20 +117,27 @@ benchmark_betamatrix <- mixture_sim_mat
 probes_select <- intersect(probes, rownames(benchmark_betamatrix))  ## 600
 library(EpiDISH)
 source("projectCellType.R")
+
 Houseman_res <- projectCellType(benchmark_betamatrix[probes_select,],as.matrix(compTable[probes_select,3:8]))
 RPC_res <- epidish(benchmark_betamatrix, as.matrix(compTable[probes_select,3:8]), method = "RPC")$estF
 CBS_res <- epidish(benchmark_betamatrix, as.matrix(compTable[probes_select,3:8]), method = "CBS")$estF
 
 corr <- rep(NA, 600)
 for (i in 1:600){
-  corr[i] <- cor(proportions_sim[i,c("Bcell", "CD4T","CD8T","Mono","Neu","NK")], RPC_res[i,], method = "spearman")
+  corr[i] <- cor(proportions_sim[i,c("Bcell", "CD4T","CD8T","Mono","Neu","NK")], CBS_res[i,], method = "spearman")
 }
 
 
 corr <- rep(NA, 6)
 for (i in 1:6){
-  corr[i] <- cor(proportions_sim[,c("Bcell", "CD4T","CD8T","Mono","Neu","NK")][,i], RPC_res[,i], method = "spearman")
+  corr[i] <- cor(proportions_sim[,c("Bcell", "CD4T","CD8T","Mono","Neu","NK")][,i],Houseman_res[,i], method = "spearman")
 }
+
+
+
+
+
+
 
 
 
@@ -106,9 +147,9 @@ load("FlowEPIC_Epithelial_nocfDNAProbePreselect_multiclassGlmnet.RData")
 source("refCompTableProbeSelection.R")
 compTable <- ref_compTable(ref_betamatrix, ref_phenotype)
 
-probes <- ProbePreselect_multiclassGlmnet[[1]][-1]
-benchmark_betamatrix <- mixture_sim_mat_highEpithelial
-
+probes <- probes_oneVsAllttest
+#probes <- ProbePreselect_multiclassGlmnet[[1]][-1]
+benchmark_betamatrix <- mixture_sim_mat
 
 probes_select <- intersect(probes, rownames(benchmark_betamatrix))  ## 600
 library(EpiDISH)
@@ -117,17 +158,17 @@ Houseman_res <- projectCellType(benchmark_betamatrix[probes_select,],as.matrix(c
 RPC_res <- epidish(benchmark_betamatrix, as.matrix(compTable[probes_select,3:9]), method = "RPC")$estF
 CBS_res <- epidish(benchmark_betamatrix, as.matrix(compTable[probes_select,3:9]), method = "CBS")$estF
 # 
-# corr <- rep(NA, 600)
-# for (i in 1:600){
-#   corr[i] <- cor(true_proportions_sim_highEpithelial[i,c("Bcell", "CD4T","CD8T","Mono","Neu","NK")], CBS_res[i,c("Bcell", "CD4T","CD8T","Mono","Neu","NK")], method = "spearman")
-# }
-# 
-# 
-# corr <- rep(NA, 6)
-# for (i in 1:6){
-#   corr[i] <- cor(true_proportions_sim_highEpithelial[,c("Bcell", "CD4T","CD8T","Mono","Neu","NK")][,i], RPC_res[,c("Bcell", "CD4T","CD8T","Mono","Neu","NK")][,i], method = "spearman")
-# }
-# 
+corr <- rep(NA, 600)
+for (i in 1:600){
+  corr[i] <- cor(proportions_sim[i,c("Bcell", "CD4T","CD8T","Mono","Neu","NK")], CBS_res[i,c("Bcell", "CD4T","CD8T","Mono","Neu","NK")], method = "spearman")
+}
+
+
+corr <- rep(NA, 6)
+for (i in 1:6){
+  corr[i] <- cor(proportions_sim[,c("Bcell", "CD4T","CD8T","Mono","Neu","NK")][,i], Houseman_res[,c("Bcell", "CD4T","CD8T","Mono","Neu","NK")][,i], method = "spearman")
+}
+
 
 
 
