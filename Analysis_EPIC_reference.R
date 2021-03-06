b893131c3cb83ba17b533ab496e3b3de596bfc14
jysong <jysongane@gmail.com>
2021-03-05 23:38:51 -0500

    Update Analysis_EPIC_reference.R

--- Analysis_EPIC_reference.R
+++ Analysis_EPIC_reference.R
@@ -1438,4 +1438,58 @@ ggplot(df) + geom_boxplot(aes(series ,value,color=group)) +
   ggtitle("GSE112308-Melanoma_Houseman-glmnetPreselect (EPICEpithelial)")
 dev.off()
 
+### GSE140038
+library(minfi)
+rgSet <- read.metharray.exp("GSE140038/idat", force = TRUE)
+
+library(GEOquery)
+geoMat<- getGEO("GSE140038")
+pD.all <- pData(geoMat[[1]])
+
+pD <- pD.all[, c("title", "geo_accession", "gender:ch1", "tissue:ch1")]
+
+
+
+grSet<- preprocessNoob(rgSet)
+getBeta(grSet)[1:3,1:3]
+head(getIslandStatus(grSet))
+betaMat <- getBeta(grSet)
+## 
+
+pD <- pD.all[, c("title", "geo_accession","age:ch1","chemotherapy any:ch1","radiationtherapy any:ch1","time point:ch1")]
+
+colnames(betaMat) <- substr(colnames(betaMat), 1, 10)
+sampleNames(rgSet_116298) <- substr(sampleNames(rgSet_116298), 1, 10)
+
+
+tissue_dat_blood <- matrix(NA, 47, 8)
+samples <- rownames(res2)
+rownames(tissue_dat_blood) <- samples
+tissue_dat_blood[,1:6] <- res2
+tissue_dat_blood[,7] <- pD[,4]
+tissue_dat_blood[,8] <- pD[,"title"]
+tissue_dat_blood <- as.data.frame(tissue_dat_blood)
+tissue_dat_blood[1:6] <- apply(tissue_dat_blood[1:6], 2, as.numeric)
+tissue_dat_blood[,7] <- as.character(tissue_dat_blood[,7])
+tissue_dat_blood[,8] <- as.character(tissue_dat_blood[,8])
+colnames(tissue_dat_blood) <- c(colnames(res2),"tissue", "title")
+
+df <- aggregate(tissue_dat_blood[,1:6],list(tissue_dat_blood$title), mean)
+df[,1] <- c(rep("High-grade glioma",12), rep("Meningioma",3))
+colnames(df)[1] <- "tissue"
+library(ggplot2)
+library(tidyr)
+df1 <- gather(df, series,value,-tissue)
+ggplot(df1) + geom_boxplot(aes(series ,value,color= tissue)) +
+  xlab('cell types')+
+  ylab('proportions') +
+  ggtitle("GSE116298-EPIC-RPC")
+
+df <- data.frame("Houseman" = corr, "RPC" = corr2, "CBS" = corr3)
+df1 <- gather(df, series,value,-tissue)
+ggplot(df1) + geom_jitter(aes(series ,value,color= tissue), position=position_jitter(0.2)) +
+  xlab('cell types')+
+  ylab('proportions') +
+  ggtitle("GSE116298-EPIC-RPC")
+
 
