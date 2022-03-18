1e07bfc85894fcfc5144431ca9f1fdb523685319
jysong <jysongane@gmail.com>
2022-03-17 22:53:49 -0600

    Update plots_2.R

--- supplementary_code/plots_2.R
+++ supplementary_code/plots_2.R
@@ -37,7 +37,23 @@ print(ggplot(data = df1 %>% gather(Deconvolution, Spearman_Correlation, -Feature
 #####################
 ### Figure 2
 #####################
+Houseman <- c(0.978, 0.947, 0.95, 0.94)
+RPC <- c(0.978, 0.938, 0.94, 0.94)
+CBS <- c(0.956, 0.950, 0.95, 0.92)
+df1 <- data.frame(FeatureSelection = c("oneVsAllttest","glmnetpreselect", "oneVsAllttest","glmnetpreselect"), 
+                  facet = c("A. EPIC reference library", "A. EPIC reference library", "B. EPIC + Epithelial reference library", "B. EPIC + Epithelial reference library"), 
+                  Houseman = Houseman, RPC = RPC, CBS = CBS)
 
+ggplot(data = df1 %>% gather(Deconvolution, Spearman_Correlation, -c(FeatureSelection,facet)), 
+       aes(x = factor(FeatureSelection, level = c("oneVsAllttest","glmnetpreselect")), y = Spearman_Correlation, fill = Deconvolution)) + 
+  geom_bar(stat = 'identity', position = 'dodge')+
+  geom_text(aes(label= round(Spearman_Correlation,2)), position = position_dodge(0.9))+
+  facet_wrap(~factor(facet,level = c("A. EPIC reference library","B. EPIC + Epithelial reference library")))+
+  labs(x = "Feature selection")+
+  labs(y = "Average Spearman correlation")+
+  labs(fill = "Deconvolution algorithms")+
+  coord_cartesian(ylim=c(0.5,1))+
+  theme(strip.text.x = element_text(size = 15))
 
 
 
