4e0325ce0711fa03d1a6363c7729c9ae41df3636
jysong <jysongane@gmail.com>
2022-07-26 15:30:00 -0700

    Create Additional_methods_TOAS.R

---
+++ supplementary_code/Additional_methods_TOAS.R
@@ -0,0 +1,82 @@
+##############################
+### Reference-free algorithms
+##############################
+load("./Downloads/Benchmark1_featureSelection.rda")
+
+library(TOAST)
+
+############# 1. most variable probes,  RefFreeEWAS
+refinx <- findRefinx(benchmark_betamatrix, nmarker = 1000)
+Y <- benchmark_betamatrix[refinx,]
+K <- 6
+outT1 <- myRefFreeCellMix(Y, mu0=myRefFreeCellMixInitialize(Y, K = K))
+estProp_RF_mostVariable <- outT1$Omega
+
+############# 2. use oneVsAllttest probes (probe_1) from reference dataset, RefFreeEWAS
+Y <- benchmark_betamatrix[probe_1,]
+K <- 6
+outT2 <- myRefFreeCellMix(Y, mu0=myRefFreeCellMixInitialize(Y, K = K))
+estProp_RF_semi <- outT2$Omega
+
+############# 3. most variable probes, improved reference-free deconvolution with cross-cell type differential analysis
+refinx <- findRefinx(benchmark_betamatrix, nmarker = 1000)
+InitNames <- rownames(benchmark_betamatrix)[refinx]
+outRF1 <- csDeconv(benchmark_betamatrix, K = K, nMarker = 1000, 
+                   InitMarker = InitNames, TotalIter = 30,
+                   bound_negative = TRUE)
+estProp_RF_mostVariable_Improved <- outRF1$estProp
+
+############# 4. use oneVsAllttest probes from reference dataset, improved reference-free deconvolution with cross-cell type differential analysis
+
+outRF2 <- csDeconv(benchmark_betamatrix, K = K, nMarker = 1000, 
+                   InitMarker = probe_1, TotalIter = 30,
+                   bound_negative = TRUE)
+estProp_RF_semi_Improved <- outRF2$estProp
+
+
+############# 5. Tsisal 
+out1 = Tsisal(benchmark_betamatrix, K = K, knowRef = NULL)
+estProp_Tsisal <- out1$estProp
+
+############# 6. semi-supervised Tsisal
+out2 = Tsisal(benchmark_betamatrix, K = K, knowRef = reference_EPIC$ref_betamatrix)
+estProp_Tsisal_semi <- out2$estProp
+
+
+
+
+##########################################
+### post-processing TOAST deconv results
+##########################################
+postprocess <- function(estProp){
+  estProp[estProp < 0] <- 0
+  res <- apply(estProp, 1, function(x){return(x/sum(x))})
+  res <- t(res)
+  return(res)
+}
+
+
+estProp_RF_mostVariable <- postprocess(estProp_RF_mostVariable)
+estProp_RF_semi <- postprocess(estProp_RF_semi)
+estProp_RF_mostVariable_Improved <- postprocess(estProp_RF_mostVariable_Improved)
+estProp_RF_semi_Improved <- postprocess(estProp_RF_semi_Improved)
+estProp_Tsisal <- postprocess(estProp_Tsisal)
+estProp_Tsisal_semi <- postprocess(estProp_Tsisal_semi)
+
+####################################################################################
+# we align the cell types from RF and RB estimations using pearson's correlation
+####################################################################################
+estProp_RF_mostVariable <- assignCellType(input = estProp_RF_mostVariable, reference = RPC_res_1) 
+colnames(estProp_RF_mostVariable) <- colnames(RPC_res_1)
+estProp_RF_semi <- assignCellType(input = estProp_RF_semi, reference = RPC_res_1) 
+colnames(estProp_RF_semi) <- colnames(RPC_res_1)
+estProp_RF_mostVariable_Improved <- assignCellType(input = estProp_RF_mostVariable_Improved, reference = RPC_res_1) 
+colnames(estProp_RF_mostVariable_Improved) <- colnames(RPC_res_1)
+estProp_RF_semi_Improved <- assignCellType(input = estProp_RF_semi_Improved, reference = RPC_res_1) 
+colnames(estProp_RF_semi_Improved) <- colnames(RPC_res_1)
+estProp_Tsisal <- assignCellType(input = estProp_Tsisal, reference = RPC_res_1) 
+colnames(estProp_Tsisal) <- colnames(RPC_res_1)
+estProp_Tsisal_semi <- assignCellType(input = estProp_Tsisal_semi, reference = RPC_res_1) 
+colnames(estProp_Tsisal_semi) <- colnames(RPC_res_1)
+
+
