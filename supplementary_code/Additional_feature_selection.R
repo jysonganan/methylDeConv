ad5a4a3f58415a66411d0f5ff5b705747d0b81c5
jysong <jysongane@gmail.com>
2022-07-31 23:22:43 -0700

    Update Additional_feature_selection.R

--- supplementary_code/Additional_feature_selection.R
+++ supplementary_code/Additional_feature_selection.R
@@ -1,3 +1,8 @@
 ## permutation feature importance
 # https://scikit-learn.org/stable/modules/permutation_importance.html
 # or Library SHAP, LIME
+
+
+## Feature Importance Ranking for Deep Learning
+# https://arxiv.org/abs/2010.08973
+# https://github.com/maksym33/FeatureImportanceDL/blob/master/example.py
